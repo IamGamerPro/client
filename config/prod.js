d39a7638edc6458cb78be73293b75e4561a67e25
kobezzza <kobezzza@gmail.com>
2015-09-07 17:21:14 +0300

    Added config

---
+++ config/prod.js
@@ -0,0 +1,20 @@
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+module.exports = {
+	css: {
+		minimize: true
+	},
+
+	uglify: {
+		beautify: true,
+		compress: {
+			warnings: false
+		}
+	}
+};
---
+++ config/stage.js
@@ -0,0 +1,13 @@
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+module.exports = {
+	snakeskin: {
+		prettyPrint: true
+	}
+};
