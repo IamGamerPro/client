9b96758b39f0249af18c58a705800475af513936
kobezzza <kobezzza@gmail.com>
2015-09-19 19:38:04 +0300

    :wrench:

--- src/core/index.js
+++ src/core/index.js
@@ -5,11 +5,3 @@
  * Released under the FSFUL license
  * https://github.com/IamGamerPro/client/blob/master/LICENSE
  */
-
-/*!
- * IamGamer.pro Client
- * https://github.com/IamGamerPro/client
- *
- * Released under the FSFUL license
- * https://github.com/IamGamerPro/client/blob/master/LICENSE
- */
