894be896df8669733c494a62fee498b75bffd4dc
kobezzza <kobezzza@gmail.com>
2016-02-22 15:04:05 +0300

    Added b-loader && p-master

---
+++ src/blocks/b-router/b-router.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+import iData from '../i-data/i-data';
+import { block, model } from '../../core/block';
+
+@model()
+@block
+export default class bRouter extends iData {}
---
+++ src/blocks/p-master/index.js
@@ -0,0 +1,11 @@
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+package('p-master')
+	.extends('i-page')
+	.dependencies('b-router');
