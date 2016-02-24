d410fcdbf3b0b2d571b69539393390de59afd22b
kobezzza <kobezzza@gmail.com>
2016-02-24 21:02:58 +0300

    Added b-group block

---
+++ src/blocks/b-group/b-group.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+import iData from '../i-data/i-data';
+import * as tpls from './b-group.ss';
+import { block, model } from '../../core/block';
+
+@model(undefined, tpls)
+@block
+export default class bGroup extends iData {}
---
+++ src/blocks/b-group/b-group.ss
@@ -0,0 +1,13 @@
+- namespace [%fileName%]
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+- include '../i-data/i-data' as placeholder
+
+- template index(params) extends ['i-data'].index
