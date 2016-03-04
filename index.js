c7983c245532fc2dc93bba8e36fed5a4ceb4e9e7
kobezzza <kobezzza@gmail.com>
2016-03-04 18:02:31 +0300

    Added static server

--- index.js
+++ index.js
@@ -8,4 +8,28 @@
  * https://github.com/IamGamerPro/client/blob/master/LICENSE
  */
 
-exports.VERSION = [0, 0, 1];
+exports.VERSION = [0, 0, 2];
+
+const
+	path = require('path');
+
+const
+	PORT = 1989,
+	DIST = path.join(__dirname, 'dist/packages');
+
+const
+	express = require('express'),
+	app = express();
+
+app
+	.use('/dist', express.static('dist'))
+	.use('/node_modules', express.static('node_modules'))
+	.use('/bower_components', express.static('bower_components'))
+	.use('/img', express.static('img'));
+
+app
+	.get('/', (req, res) => res.sendFile(path.join(DIST, 'p-auth.html')))
+	.get('/**', (req, res) => res.sendFile(path.join(DIST, 'p-master.html')))
+	.listen(PORT);
+
+console.log(`Server started at localhost:${PORT}`);
