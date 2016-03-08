20ac4416d47b6e4bc4428ee6879ea12745da66f8
kobezzza <kobezzza@gmail.com>
2016-03-08 14:41:56 +0300

    Added i-dynamic-page

---
+++ src/blocks/i-dynamic-page/i-dynamic-page.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+import iBlock from '../i-block/i-block';
+import { block, model } from '../../core/block';
+
+@model()
+@block
+export default class iDynamicPage extends iBlock {}
