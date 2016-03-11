590b6dbaf83ca8232f9c7f33debfcc4a222e3125
kobezzza <kobezzza@gmail.com>
2016-03-11 18:35:32 +0300

    Added b-scroll && b-textarea

---
+++ src/blocks/b-scroll/b-scroll.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+import iBlock from '../i-block/i-block';
+import * as tpls from './b-scroll.ss';
+import { block, model } from '../../core/block';
+
+@model({}, tpls)
+@block
+export default class bScroll extends iBlock {}
---
+++ src/blocks/b-textarea/b-textarea.ss
@@ -0,0 +1,13 @@
+- namespace [%fileName%]
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+- include '../b-input/b-input' as placeholder
+
+- template index(params) extends ['b-input'].index
