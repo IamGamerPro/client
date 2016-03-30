0a9009a9e2ea7cb3172225910db681ab6ff206cc
kobezzza <kobezzza@gmail.com>
2016-03-30 14:30:53 +0300

    Refactoring

---
+++ src/blocks/b-crop/modules/resize.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+export default {
+
+};
