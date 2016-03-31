cf03f85102f8c0b0725ae0f3fce2c78a60f383fa
kobezzza <kobezzza@gmail.com>
2016-03-31 19:38:29 +0300

    Added getOffset

--- src/core/dom.js
+++ src/core/dom.js
@@ -54,3 +54,34 @@ Element.prototype.getPosition = function (): {top: number, left: number} {
 		left: box.left + pageXOffset
 	};
 };
+
+/**
+ * Returns a position of the element relative to the parent
+ * @param [parent]
+ */
+Node.prototype.getOffset = function (parent?: Element | string): {top: number, left: number} {
+	const res = {
+		top: this.offsetTop,
+		left: this.offsetLeft
+	};
+
+	if (!parent) {
+		return res;
+	}
+
+	let
+		{offsetParent} = this;
+
+	function matcher() {
+		return offsetParent && offsetParent !== document.documentElement &&
+			(Object.isString(parent) ? !offsetParent.matches(parent) : offsetParent !== parent);
+	}
+
+	while (matcher()) {
+		res.top += offsetParent.offsetTop;
+		res.left += offsetParent.offsetLeft;
+		({offsetParent} = offsetParent);
+	}
+
+	return res;
+};
