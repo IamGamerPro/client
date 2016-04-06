e754e3fcc6d774643d27b25157f464b06326cc1e
kobezzza <kobezzza@gmail.com>
2016-04-06 16:04:41 +0300

    Added b-avatar-uploader

---
+++ src/blocks/b-avatar-uploader/b-avatar-uploader.ss
@@ -0,0 +1,13 @@
+- namespace [%fileName%]
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+- include '../b-window/' as placeholder
+
+- template index(params) extends ['b-window'].index
---
+++ src/blocks/b-avatar-uploader/index.js
@@ -0,0 +1,11 @@
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+package('b-avatar-uploader')
+	.extends('b-window')
+	.dependencies('b-button', 'b-form', 'b-image-editor', 'b-progress', 'b-uploader');
