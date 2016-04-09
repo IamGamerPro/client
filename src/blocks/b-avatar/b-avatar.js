5d4d6f83bbe039f054f813c52fc947e060c9bc52
kobezzza <kobezzza@gmail.com>
2016-04-09 13:49:36 +0300

    Added b-avatar

---
+++ src/blocks/b-avatar/b-avatar.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+import iData from '../i-data/i-data';
+import * as tpls from './b-avatar.ss';
+import { block, model } from '../../core/block';
+
+@model({}, tpls)
+@block
+export default class bAvatar extends iData {}
---
+++ src/blocks/b-avatar/index.js
@@ -0,0 +1,11 @@
+/*!
+ * IamGamer.pro Client
+ * https://github.com/IamGamerPro/client
+ *
+ * Released under the FSFUL license
+ * https://github.com/IamGamerPro/client/blob/master/LICENSE
+ */
+
+package('b-avatar')
+	.extends('i-data')
+	.dependencies('b-pseudo-link');
